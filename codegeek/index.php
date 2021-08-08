a366a2bdc6ae31a2e772de39a598c2e7905bfd71
Yekinni Basiru Kolawole <82812266+yekinnibasiru@users.noreply.github.com>
2021-08-08 01:31:00 -0700

    First commit

---
+++ codegeek/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codegeek | Where developers meet</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="container site-container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="round-box">
+                    <h2 id="site-title"><a href="index.php">CODEGEEK FORUM</a></h2>
+                    <p>Welcome, <b style="color: brown;">Guest:</b>    
+                    
+                    <a href="#">Join Codegeek- </a>
+                    <a href="#"><b>LOGIN!- </b></a>
+                    <a href="#">Trending- </a> 
+                    <a href="#">Recent- </a>
+                    <a href="#">New</a>
+                </p>
+                <p><b style="color: brown;">Statistics:</b> <b>2,716,469</b>     <b style="color: brown;">Topics:</b> <b>6,416,469</b> </p>
+                <p><b style="color: brown;">Date:</b> Wednesday,04 August 2021 @ 06:53 PM</p>
+                <form action="search.php" method="get" id="search-form">
+                    <input type="text" name="keyword" id="search-input">
+                    <input type="submit" name="search" id="search-button" value="Search">
+                </form>
+                </div>
+                <div class="section">
+                    <h2 class="section-header">CODEGEEK CATEGORIES</h2>
+                    <p>
+                        <a href="#" style="font-weight:bold;">Programming/ Web development:</a>
+                        <a href="#" class="category-link">Html</a>
+                        <a href="#" class="category-link">Css</a>
+                        <a href="#" class="category-link">Javascript</a>
+                        <a href="#" class="category-link">Jquery</a>
+                        <a href="#" class="category-link">Mongodb</a>
+                        <a href="#" class="category-link">Mysql</a>
+                        <a href="#" class="category-link">Php</a>
+                        <a href="#" class="category-link">C++</a>
+                        <a href="#" class="category-link">C-sharp</a>
+                        <a href="#" class="category-link">Redis</a>
+                        <a href="#" class="category-link">API Development</a>
+                        <a href="#" class="category-link">Nodejs</a>
+                        <a href="#" class="category-link">Expressjs</a>
+                        <a href="#" class="category-link">Laravel</a>
+                        <a href="#" class="category-link">Codeigniter</a>
+                    </p>
+                    <p class="section-header" style="border-radius:0; text-transform:unset;">
+                         <a href="#">Jobs/Interns:</a>
+                         <a class="category-link" href="#">Web Developer</a>
+                         <a class="category-link" href="">Data scientist</a>
+                         <a class="category-link" href="">Cyber Security Expert</a>
+                         <a class="category-link" href="">Data Analyst</a>
+                         <a class="category-link" href="">IT Manager</a>
+                         <a class="category-link" href="">Database Manager</a>
+                         <a class="category-link" href="">UI/UX Designer</a>
+                         <a class="category-link" href="">Networking</a>
+                    </p>
+                    <p>
+                         <a href="#" style="font-weight:bold;">Courses/Blog:</a>
+                         <a class="category-link" href="#">Web Developer Course</a>
+                         <a class="category-link" href="">Data scientist Course</a>
+                         <a class="category-link" href="">Cyber Security Course</a>
+                         <a class="category-link" href="">Data Analyst Course</a>
+                         <a class="category-link" href="">IT Manager Course</a>
+                         <a class="category-link" href="">Database Manager Course</a>
+                         <a class="category-link" href="">UI/UX Designer Course</a>
+                    </p>
+                </div>
+                <div class="section">
+                    <h2 class="section-header">
+                        😃
+                        <a href="#">Latest Thread -</a>
+                        <a href="#">Advertisement -</a>
+                        <a href="#">How to</a>
+                        😃
+                    </h2>
+                    <p><a href="#" class="post-link">What to know as a backend developer</a></p>
+                    <p><a href="#" class="post-link">Securing Mysql Database During Development</a></p>
+                    <p><a href="#" class="post-link">UI/UX in world developer market</a></p>
+                    <p><a href="#" class="post-link">What is it happening in web developer world?</a></p>
+                    <p><a href="#" class="post-link">What is UI/UX a better way for design approach</a></p>
+                    <p><a href="#" class="post-link">Learn nodejs and Expressjs</a></p>
+                    <p><a href="#" class="post-link">Taking a great step to tech startup</a></p>
+                    <p><a href="#" class="post-link">Learn advanced PHP with OOP aspect</a></p>
+                    <p><a href="#" class="post-link">Learn CSS3 with its advanced features</a></p>
+                    <p><a href="#" class="post-link">Learn NODEJS a Javascript runtime environment for backend</a></p>
+                    <p><a href="#" class="post-link">Learn database management</a></p>
+                    <p><a href="#" class="post-link">Learn Networking</a></p>
+                    <p class="section-footer">
+                        <a href="#">1</a>
+                        <a href="#">2</a>
+                        <a href="#">3</a>
+                        <a href="#">4</a>
+                        <a href="#">5</a>
+                        <a href="#">4</a>
+                        <a href="#">7</a>
+                        <a href="#">9</a>
+                        <a href="#">...</a>
+                        <a href="#">200</a>
+                    </p>
+                </div>
+                <div class="section p-2">
+                    <h5 class="text-center m-0 mt-1">Online Member:</h5>
+                    <p style="margin-top:3px;"> <b>3047 members</b> and <b>6647 guests</b> are online in the last <b>10 minutes</b></p>
+                </div>
+                <div class="text-center">
+                    <a href="#"> <span></span> Jump Up</a>
+                </div>
+                <div class="section footer p-2">
+                    <form action="search.php" method="get" id="search-form">
+                        <input type="text" name="keyword" id="search-input">
+                        <input type="submit" name="search" id="search-button" value="Search">
+                    </form>
+                    <p> Copyright &copy; <?php echo date('Y') ?>- By Yekinni Adekola, Build with <span style="color:red;">❤</span> . <b>All rights reserved.</b> </p>
+                    <p> <b>Disclaimer:</b> Every Codegeek member is  <b>solely responsible</b>  for <b>anything</b> he/she posts on this platform </p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
